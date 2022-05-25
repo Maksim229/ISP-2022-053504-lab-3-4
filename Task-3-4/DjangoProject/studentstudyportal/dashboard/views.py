200a13bcd4f8d47089be3bd0263925e95b52146e
Gleb Zikunov <zikunovga@gmail.com>
2022-05-26 01:47:44 +0300

    feat: add Homework table

--- Task-3-4/DjangoProject/studentstudyportal/dashboard/views.py
+++ Task-3-4/DjangoProject/studentstudyportal/dashboard/views.py
@@ -1,7 +1,7 @@
-from email import message
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . forms import *
 from django.contrib import messages
+from django.views import generic
 
 # Create your views here.
 def home(request):
@@ -22,3 +22,61 @@ def notes(request):
     notes = Notes.objects.filter(user=request.user)
     context = {'notes':notes, 'form':form}
     return render(request, 'dashboard/notes.html', context)    
+
+def delete_note(request, pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")
+
+class NotesDetailView(generic.DetailView):
+    model = Notes
+
+def homework(request):
+    if request.method == "POST":
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST['is_finished']
+                if finished == 'on':
+                    finished = True
+                else:
+                    finished = False
+            except:
+                finished = False
+            homeworks = Homework(
+                user = request.user,
+                subject = request.POST['subject'],
+                title = request.POST['title'],
+                description = request.POST['description'],
+                due = request.POST['due'],
+                is_finished = finished
+            )
+            homeworks.save()
+        messages.success(request,f'Homework Added from {request.user.username}!')
+    else:
+        form = HomeworkForm()
+    homework = Homework.objects.filter(user=request.user)
+    
+    if len(homework) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+
+    context = {
+        'homeworks':homework,
+        'homeworks_done':homework_done,
+        'form':form,
+    }
+    return render(request, 'dashboard/homework.html', context)
+
+def update_homework(request, pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect("homework")
+
+def delete_homework(request, pk=None):
+    Homework.objects.get(id=pk).delete()
+    return redirect("homework")
