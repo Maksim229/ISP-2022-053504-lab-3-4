76ba5fbea63331ed1b9606c11e72cf9afc44f06e
Gleb Zikunov <zikunovga@gmail.com>
2022-05-26 22:30:21 +0300

    feat: added YouTube API, Todo table

--- Task-3-4/DjangoProject/studentstudyportal/dashboard/forms.py
+++ Task-3-4/DjangoProject/studentstudyportal/dashboard/forms.py
@@ -19,3 +19,11 @@ class HomeworkForm(forms.ModelForm):
             'description',
             'due',
             'is_finished']
+
+class DashboardForm(forms.Form):
+    text = forms.CharField(max_length=100, label="Enter Your search: ")
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'is_finished']
--- Task-3-4/DjangoProject/studentstudyportal/dashboard/views.py
+++ Task-3-4/DjangoProject/studentstudyportal/dashboard/views.py
@@ -1,11 +1,15 @@
+from multiprocessing import context
+from unittest import result
 from django.shortcuts import redirect, render
 from . forms import *
 from django.contrib import messages
 from django.views import generic
+from youtubesearchpython import VideosSearch
 
 # Create your views here.
 def home(request):
     return render(request, 'dashboard/home.html')
+
 
 def notes(request):
     if request.method == "POST":
@@ -23,12 +27,15 @@ def notes(request):
     context = {'notes':notes, 'form':form}
     return render(request, 'dashboard/notes.html', context)    
 
+
 def delete_note(request, pk=None):
     Notes.objects.get(id=pk).delete()
     return redirect("notes")
 
+
 class NotesDetailView(generic.DetailView):
     model = Notes
+
 
 def homework(request):
     if request.method == "POST":
@@ -68,6 +75,7 @@ def homework(request):
     }
     return render(request, 'dashboard/homework.html', context)
 
+
 def update_homework(request, pk=None):
     homework = Homework.objects.get(id=pk)
     if homework.is_finished == True:
@@ -77,6 +85,90 @@ def update_homework(request, pk=None):
     homework.save()
     return redirect("homework")
 
+
 def delete_homework(request, pk=None):
     Homework.objects.get(id=pk).delete()
     return redirect("homework")
+
+
+def youtube(request):
+    if request.method == "POST":
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        video = VideosSearch(text, limit=10)
+        result_list = []
+        for i in video.result()['result']:
+            result_dict = {
+                'input':text,
+                'title':i['title'],
+                'duration':i['duration'],
+                'thumbnail':i['thumbnails'][0]['url'],
+                'channel':i['channel']['name'],
+                'link':i['link'],
+                'views':i['viewCount']['short'],
+                'published':i['publishedTime'],
+            }
+            desc =''
+            if i['descriptionSnippet']:
+                for j in i['descriptionSnippet']:
+                    desc += j['text']
+            result_dict['description'] = desc
+            result_list.append(result_dict)
+            context = {
+                'form':form,
+                'results':result_list
+            }
+        return render(request, 'dashboard/youtube.html', context)    
+    else:    
+        form = DashboardForm()
+    context = {'form':form}
+    return render(request, "dashboard/youtube.html", context)
+
+
+def todo(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST["is_finished"]
+                if finished == 'on':
+                    finished = True
+                else:
+                    finished = False
+            except:
+                finished = False
+            todos = Todo(
+                user = request.user,
+                title = request.POST['title'],
+                is_finished = finished  
+            )
+            todos.save()
+        messages.success(request,f"Todo Added from {request.user.username} !")
+    else:
+        form = TodoForm()
+    todo = Todo.objects.filter(user=request.user)
+    if len(todo) == 0:
+        todos_done = True
+    else:
+        todos_done = False
+    context = {
+        'form':form,
+        'todos':todo,
+        'todos_done':todos_done
+    }
+    return render(request, "dashboard/todo.html", context)
+
+
+def update_todo(request, pk=None):
+    todo = Todo.objects.get(id=pk)
+    if todo.is_finished == True:
+        todo.is_finished = False
+    else:
+        todo.is_finished = True
+    todo.save()
+    return redirect('todo')
+
+
+def delete_todo(request, pk=None):
+    Todo.objects.get(id=pk).delete()
+    return redirect("todo")
