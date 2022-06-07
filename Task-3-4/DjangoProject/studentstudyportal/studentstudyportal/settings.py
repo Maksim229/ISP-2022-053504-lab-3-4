2a8b2fd7bedad9de508326b725ac0122fe5ae2ca
Gleb Zikunov <zikunovga@gmail.com>
2022-06-07 14:42:07 +0300

    feat: added PostgreDb on Heroku

--- Task-3-4/DjangoProject/studentstudyportal/studentstudyportal/settings.py
+++ Task-3-4/DjangoProject/studentstudyportal/studentstudyportal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,13 +76,14 @@ WSGI_APPLICATION = 'studentstudyportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {'default': dj_database_url.config('DATABASE_URL')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
